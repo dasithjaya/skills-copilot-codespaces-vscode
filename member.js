0f72a239bb687ee973da4a2c01bc8da4cd4da039
dasithjaya <89839140+dasithjaya@users.noreply.github.com>
2023-08-02 11:52:19 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,13 @@
+function skillsMember() {
+  var skills = document.getElementById("skills");
+  var skillsMember = document.getElementById("skillsMember");
+  var skillsMemberValue = skillsMember.value;
+  var skillsMemberText = skillsMember.options[skillsMember.selectedIndex].text;
+  var skillsMemberOption = document.createElement("option");
+  skillsMemberOption.text = skillsMemberText;
+  skillsMemberOption.value = skillsMemberValue;
+  skillsMemberOption.selected = true;
+  skills.add(skillsMemberOption);
+  skillsMember.remove(skillsMember.selectedIndex);
+  skillsMember.selectedIndex = 0;
+}
